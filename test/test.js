42a04561f3aa24eef555281699cce0c01a9533a8
Jan Lehnardt <jan@apache.org>
2015-01-28 14:25:18 +0100

    more tests

--- test/test.js
+++ test/test.js
@@ -10,3 +10,24 @@ assert.deepEqual(result, [{
   key: null,
   value: {a: 1}
 }]);
+
+
+// should bail if fixture is missing
+try {
+  var test2 = new CouchappTest({
+    src: 'test/fixture/mapjs'
+  });
+  assert(false); // should not happen
+} catch(e) {
+  assert.equal(e.message, 'missing options.fixture argument');
+}
+
+// should bail if src is missing
+try {
+  var test2 = new CouchappTest({
+    fixture: { a: 1}
+  });
+  assert(false); // should not happen
+} catch(e) {
+  assert.equal(e.message, 'missing options.src argument');
+}
